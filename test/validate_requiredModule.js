c29f39d79e80f4a6d81b1fd6fef0bc1c22e67d24
angelozerr <angelo.zerr@gmail.com>
2015-09-03 03:58:26 +0200

    Validate require param type.

--- test/validate_requiredModule.js
+++ test/validate_requiredModule.js
@@ -1,6 +1,16 @@
 var util = require("./util-lint");
 
 exports['test required module validation'] = function() {
+
+  // bad type
+  util.assertLint("require(10)", {
+    messages : [{"message":"Invalid argument at 1: cannot convert from number to string",
+      "from":8,
+      "to":10,
+      "severity":"error",
+      "file":"test1.js"}
+    ]
+  });
   
   // Unknown module 'XXX'
   util.assertLint("require('XXX')", {
